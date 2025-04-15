5c7fc4abb3b126594cfccbc448ad5830cf41f35b
Axel Cabral <axel.cabral0102@gmail.com>
2025-04-15 00:25:07 -0300

    Auth

--- app/(project)/dashboard/page.tsx
+++ app/(project)/dashboard/page.tsx
@@ -1,7 +1,17 @@
-export default function Dashboard() {
+import { auth } from '@/app/lib/auth';
+import { redirect } from 'next/navigation';
+
+export default async function Dashboard() {
+  const session = await auth();
+
+  if(!session){
+    redirect("/login");
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Pretected Dashboard</h1>
+      <p>Email do usuário: {session?.user?.email ? session?.user?.email : "Não está logado"}</p>
     </div>
   );
 }
